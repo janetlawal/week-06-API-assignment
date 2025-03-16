c37561ade8c6c8fc5fc91a30abe1a58debf624f5
janet lawal <janetlawall@yahoo.co.uk>
2025-03-16 22:26:55 +0000

    changed fetch URL to render server

--- client/app.js
+++ client/app.js
@@ -5,7 +5,13 @@ myForm.addEventListener("submit", sendChatRequest);
 async function sendChatRequest(event) {event.preventDefault();
 console.log("the promt is:", userPromt);
 
-const response = await fetch("http://localhost:8080/chat", {});
+const response = await fetch("https://week-06-api-assignment.onrender.com/", {
+    method: "POST",
+    headers: {
+        "content-Type": "application/json",
+    },
+    body: JSON.stringify[{ userPromt }],
+});
 const data = await response.json()
 console.log("The data recieved back is",data);
 const responseP = document.createElement["p"];
